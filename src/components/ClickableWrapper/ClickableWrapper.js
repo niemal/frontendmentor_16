7c66a7b3d427401869b585b083c808614dc816cd
Nick C <niemal@users.noreply.github.com>
2023-03-29 20:14:03 +0300

    accessibility visual improvement

--- src/components/ClickableWrapper/ClickableWrapper.js
+++ src/components/ClickableWrapper/ClickableWrapper.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ClickableWrapper = ({ children, onClick, ...props }) => {
+  const [isKeyboardFocused, setIsKeyboardFocused] = useState(false);
   const handleKeyDown = (event) => {
     if (event.key === " " || event.key === "Enter") {
       event.preventDefault();
@@ -9,6 +10,40 @@ const ClickableWrapper = ({ children, onClick, ...props }) => {
         onClick(event);
       }
     }
+
+    if (firstChild.props.onKeyDown) {
+      firstChild.props.onKeyDown(event);
+    }
+
+    if (event.key === "Tab") {
+      setIsKeyboardFocused(true);
+    }
+  };
+
+  const handleMouseDown = (e) => {
+    if (firstChild.props.onMouseDown) {
+      firstChild.props.onMouseDown(e);
+    }
+
+    setIsKeyboardFocused(false);
+  };
+
+  const handleBlur = (e) => {
+    if (firstChild.props.onBlur) {
+      firstChild.props.onBlur(e);
+    }
+
+    setIsKeyboardFocused(false);
+  };
+
+  const handleKeyUp = (e) => {
+    if (firstChild.props.onKeyUp) {
+      firstChild.props.onKeyUp(e);
+    }
+
+    if (e.key === "Tab") {
+      setIsKeyboardFocused(true);
+    }
   };
 
   const firstChild = React.Children.toArray(children)[0];
@@ -16,7 +51,11 @@ const ClickableWrapper = ({ children, onClick, ...props }) => {
     tabIndex: "0",
     role: "button",
     onKeyDown: handleKeyDown,
+    onKeyUp: handleKeyUp,
+    onMouseDown: handleMouseDown,
+    onBlur: handleBlur,
     onClick,
+    isKeyboardFocused,
     ...props,
     ...firstChild.props,
   });
